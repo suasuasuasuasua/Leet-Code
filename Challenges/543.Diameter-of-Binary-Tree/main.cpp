c83dc6522516a72d2fc936364391b6e3a230b00f
Justin Hoang <justinhoang@mines.edu>
2021-10-10 19:35:59 -0600

    543

---
+++ Challenges/543.Diameter-of-Binary-Tree/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode () : val(0), left(nullptr), right(nullptr) {}
+    TreeNode (int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode (int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+
+        return diameter;
+    }
+};
+int main() {
+   Solution solution;
+   TreeNode* root = new TreeNode();
+
+   cout << solution.diameterOfBinaryTree(root) << endl;
+}
